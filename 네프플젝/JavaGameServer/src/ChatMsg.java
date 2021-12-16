404c217c6c06d6b96e0ec866d6cd8fd0572b7e81
jaeseok <jssjchl@naver.com>
2021-12-16 16:15:15 +0900

    commit

--- 네프플젝/JavaGameServer/src/ChatMsg.java
+++ 네프플젝/JavaGameServer/src/ChatMsg.java
@@ -1,6 +1,5 @@
 
 // ChatMsg.java 채팅 메시지 ObjectStream 용.
-import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.io.Serializable;
 import javax.swing.ImageIcon;
@@ -8,6 +7,7 @@ import javax.swing.ImageIcon;
 class ChatMsg implements Serializable {
 	private static final long serialVersionUID = 1L;
 	public String code; // 100:로그인, 400:로그아웃, 200:채팅메시지, 300:Image, 500: Mouse Event
+						// 600: 색상바꾸기 700: 화면 클리어 800: 게임시작 및 게임관련 메시지 900: 펜사이즈 증감
 	public String UserName;
 	public String data;
 	public String gStatus;
